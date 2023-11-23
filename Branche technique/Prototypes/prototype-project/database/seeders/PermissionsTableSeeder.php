4dd464b419d2be0a5d350b3c7500a052e0bfb262
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-11-23 15:28:06 +0100

    update prototype permission seeder

--- Branche technique/Prototypes/prototype-project/database/seeders/PermissionsTableSeeder.php
+++ Branche technique/Prototypes/prototype-project/database/seeders/PermissionsTableSeeder.php
@@ -9,35 +9,20 @@ class PermissionsTableSeeder extends Seeder
 {
     public function run()
     {
-        Permission::create(['name' => 'create-ProjectController']);
-        Permission::create(['name' => 'store-ProjectController']);
-        Permission::create(['name' => 'show-ProjectController']);
-        Permission::create(['name' => 'edit-ProjectController']);
-        Permission::create(['name' => 'update-ProjectController']);
-        Permission::create(['name' => 'destroy-ProjectController']);
-        Permission::create(['name' => 'index-ProjectController']);
-        Permission::create(['name' => 'import-ProjectController']);
-        Permission::create(['name' => 'export-ProjectController']);
+        $controllers = ['Project', 'Task', 'Member']; // Add more controllers as needed
 
+        foreach ($controllers as $controller) {
+            $this->createPermissionsForController($controller);
+        }
+    }
 
-        Permission::create(['name' => 'create-TaskController']);
-        Permission::create(['name' => 'store-TaskController']);
-        Permission::create(['name' => 'show-TaskController']);
-        Permission::create(['name' => 'edit-TaskController']);
-        Permission::create(['name' => 'update-TaskController']);
-        Permission::create(['name' => 'destroy-TaskController']);
-        Permission::create(['name' => 'index-TaskController']);
-        Permission::create(['name' => 'import-TaskController']);
-        Permission::create(['name' => 'export-TaskController']);
+    private function createPermissionsForController($controller)
+    {
+        $actions = ['create', 'store', 'show', 'edit', 'update', 'destroy', 'index', 'import', 'export'];
 
-
-        Permission::create(['name' => 'create-MemberController']);
-        Permission::create(['name' => 'store-MemberController']);
-        Permission::create(['name' => 'show-MemberController']);
-        Permission::create(['name' => 'edit-MemberController']);
-        Permission::create(['name' => 'update-MemberController']);
-        Permission::create(['name' => 'destroy-MemberController']);
-        Permission::create(['name' => 'index-MemberController']);
-
+        foreach ($actions as $action) {
+            $permissionName = $action . '-' . $controller . 'Controller';
+            Permission::create(['name' => $permissionName]);
+        }
     }
 }
